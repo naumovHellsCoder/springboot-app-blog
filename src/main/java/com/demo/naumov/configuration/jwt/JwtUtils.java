018c94a336b618da599d688b46e0ee4f8142b657
naumovHellsCoder <naumov0coder@gmail.com>
2022-10-01 00:05:39 +0300

    new commit

---
+++ src/main/java/com/demo/naumov/configuration/jwt/JwtUtils.java
@@ -0,0 +1,21 @@
+package com.demo.naumov.configuration.jwt;
+
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import org.springframework.stereotype.Component;
+
+import java.util.Date;
+
+@Component
+public class JwtUtils {
+    private Long jwtExpirationMs = 868000L;
+    private String jwtSecret = "jwt-secret";
+
+    private String generationToken(String username){
+        return Jwts.builder().setExpiration(new Date())
+                .setIssuedAt(new Date(new Date().getTime() + jwtExpirationMs))
+                .setSubject(username)
+                .signWith(SignatureAlgorithm.HS512, jwtSecret)
+                .compact();
+    }
+}
